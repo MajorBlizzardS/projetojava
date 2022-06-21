001f99cf1f3fda95a48d50c49bf0a7ee4ddad1aa
Rodrigo Guerra <rodrigo.rgas98@gmail.com>
2022-06-20 21:11:55 -0300

    Adicionando arquivos
    Preparando arquivos para prova

---
+++ segundaetapaprova/src/main/java/segundaetapaprova/Segundaetapaprova.java
@@ -0,0 +1,10 @@
+package segundaetapaprova;
+
+public class Segundaetapaprova {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    
+    }
+}
